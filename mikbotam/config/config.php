c986ecc03c3bb32b97f287b2ced36031f61ef8ce
kakanet <52841812+kakanet@users.noreply.github.com>
2020-06-25 09:12:55 +0800

    Add files via upload

---
+++ mikbotam/config/config.php
@@ -0,0 +1,7 @@
+<?php
+$datasa["ipaddress"] 	   = "vpn.labkom.my.id:8242";                     //Ubah ip router anda            wajib
+$datasa["user"] 				= "umum";                 //ubah username mirktoik       wajib
+$datasa["password"] 	    	= "umum";                //paswword router      wajib
+$datasa["token"] 		    	= "1006308932:AAG_cXzU6cMkvIQE_-ipSX-Z0yfcNZCfTwQ";                //token Bot    wajib
+$datasa["usernamebot"]     = "@kknetselongan_bot";           //usernamebot wajib
+$datasa["id"] 			    	= "Kosongisajapaswwordbotcomingsoon";   //paswwordbot comingsoon
