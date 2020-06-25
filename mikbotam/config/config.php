111644eb129ac15d39105c4b60c657e27f559076
kakanet <52841812+kakanet@users.noreply.github.com>
2020-06-25 09:21:01 +0800

    Update config.php

--- mikbotam/config/config.php
+++ mikbotam/config/config.php
@@ -2,6 +2,6 @@
 $datasa["ipaddress"] 	   = "vpn.labkom.my.id:8242";                     //Ubah ip router anda            wajib
 $datasa["user"] 				= "umum";                 //ubah username mirktoik       wajib
 $datasa["password"] 	    	= "umum";                //paswword router      wajib
-$datasa["token"] 		    	= "1006308932:AAG_cXzU6cMkvIQE_-ipSX-Z0yfcNZCfTwQ";                //token Bot    wajib
-$datasa["usernamebot"]     = "@kknetselongan_bot";           //usernamebot wajib
+$datasa["token"] 		    	= "1030520822:AAGazAoaRsp4cw_53cMvQ9qhx1z2WUj-Oak";                //token Bot    wajib
+$datasa["usernamebot"]     = "@netselongan_bot";           //usernamebot wajib
 $datasa["id"] 			    	= "Kosongisajapaswwordbotcomingsoon";   //paswwordbot comingsoon
